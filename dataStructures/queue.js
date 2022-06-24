fed818c11252b1c0dd0be6342cd98c2c7b8aee97
Joe Zhou <zhou_tianwei@outlook.com>
2022-06-24 00:10:41 -0400

    priority queue impl by array

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -22,4 +22,43 @@ let Queue = function () {
     };
 };
 
-module.exports = { Queue: Queue };
+//Implementation with array
+let PriorityQueue = function () {
+    this.dataStore = [];
+
+    this.enqueue = (element) => {
+        if (this.dataStore.length === 0) {
+            this.dataStore.push(element);
+        } else {
+            let added = false;
+            for (let i = 0; i < this.dataStore.length; i++) {
+                if (this.dataStore[i][1] < element[1]) {
+                    this.dataStore.splice(i, 1, element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                this.dataStore.push(element);
+            }
+        }
+    };
+
+    this.front = () => {
+        return this.dataStore[0][0];
+    };
+
+    this.dequeue = () => {
+        this.dataStore.shift();
+    };
+
+    this.size = () => {
+        return this.dataStore.length;
+    };
+
+    this.isEmpty = () => {
+        return this.dataStore.length === 0;
+    };
+};
+
+module.exports = { Queue: Queue, PriorityQueue: PriorityQueue };
